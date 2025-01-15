5a0cb75c4a35f6b03988486726a254e0bcd49ebd
Jake <Jake@LAPTOP-FAA0D0E9.dhcp.wustl.edu>
2025-01-15 15:27:08 -0600

    Done again

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" That's how the cookie crumbles! ");
+		System.out.println(" That's how the cookie crumbless! ");
 	}   
 }
